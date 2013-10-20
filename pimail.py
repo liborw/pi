4a7840a8bcdffa420fb6a2bf7412b19eb5a11047
Libor Wagner <liborwagner@gmail.com>
2013-10-20 21:23:43 +0000

    Add subject

--- pimail.py
+++ pimail.py
@@ -1,7 +1,14 @@
 import smtplib
+from email.mime.text import MIMEText
+
 import conf
+
+msg = MIMEText("It works!")
+msg['Subject'] = 'RPi: Startup Message'
+msg['From'] = conf.GMAIL_MAIL
+msg['To'] = conf.GMAIL_MAIL
 
 server = smtplib.SMTP("smtp.gmail.com:587")
 server.starttls()
 server.login(conf.GMAIL_USER,conf.GMAIL_PASS)
-server.sendmail(conf.GMAIL_MAIL, conf.GMAIL_MAIL, "It works!")
+server.sendmail(conf.GMAIL_MAIL, conf.GMAIL_MAIL, msg.as_string())
