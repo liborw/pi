1574d6120148c2cc93ece16234264e7ab377390b
Libor Wagner <liborwagner@gmail.com>
2013-10-20 21:36:37 +0000

    Add piblic IP to the report

--- pimail.py
+++ pimail.py
@@ -1,14 +1,17 @@
 import smtplib
 from email.mime.text import MIMEText
 import subprocess
+import urllib2
 
 import conf
 
 co = subprocess.Popen(['ifconfig'], stdout = subprocess.PIPE)
 ifconfig = co.stdout.read()
+my_ip = urllib2.urlopen('http://ident.me/').read()
 
 text  = "\n$ ifconfig\n"
 text += ifconfig
+text += "\nPublic IP: " + my_ip + "\n"
 
 msg = MIMEText(text)
 msg['Subject'] = 'RPi: Startup Message'
